b95474badecc7f5cdeb214082dc2a372b01aa39d
Satyajit Kamble <satyajitkamble97@gmail.com>
2021-11-07 12:31:49 -0800

    ADDED BOTH

--- file.py
+++ file.py
@@ -1,2 +1,4 @@
 print("Satyajit learning how to use git")
 print("added my second change------hahahhaa")
+print("added my second change")
+print("Ami made some code changes on remote-----hahahahah")
